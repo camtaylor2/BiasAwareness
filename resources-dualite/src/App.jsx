0d25cce8d922c346c852c2fd9a86a9d006719310
camtaylor2 <camtaylor245@gmail.com>
2024-03-25 16:22:03 -0400

    Resource page

---
+++ resources-dualite/src/App.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "./animation.css";
+import runAnimations, { allLinks, allFunctions } from "./scripts";
+const App = () => {
+  useEffect(() => {
+    runAnimations();
+  }, []);
+  return (
+    <div className="parent-div">
+      <div className="resources-1-261985 pos-abs" id="id-38">
+        <div className="rectangle-5-1-578136 pos-abs" id="id-412"></div>
+        <div className="resources-1-143071 pos-abs" id="id-10338">
+          <span className="resources-1-143071-0">{"Resources"}</span>
+        </div>
+        <div className="resources-for-k-1-254760 pos-abs" id="id-12344">
+          <span className="resources-for-k-1-254760-0">
+            {"Resources for K-12 educators and college/university professors"}
+          </span>
+        </div>
+        <div className="test-your-bias-1-111936 pos-abs" id="id-13547">
+          <span className="test-your-bias-1-111936-0">
+            {"Test Your Bias "} <br />{" "}
+          </span>
+        </div>
+        <div className="recognizing-mic-1-31616 pos-abs" id="id-13829">
+          <span className="recognizing-mic-1-31616-0">
+            {"Recognizing Microaggressions"}
+          </span>
+        </div>
+        <div className="bias-training-i-1-121128 pos-abs" id="id-13833">
+          <span className="bias-training-i-1-121128-0">
+            {"Bias training ideas for organizations"}
+          </span>
+        </div>
+        <div className="understanding-y-1-5600 pos-abs" id="id-14028">
+          <span className="understanding-y-1-5600-0">
+            {"Understanding your racial bias"}
+          </span>
+        </div>
+        <div className="the-similartome-1-203870 pos-abs" id="id-14037">
+          <span className="the-similartome-1-203870-0">
+            {"The Similar-to-Me Effect"}
+          </span>
+        </div>
+        <div className="header-1-416208-container pos-abs" id="id-1429">
+          <div className="header-1-173436 pos-abs" id="id-1429">
+            <div className="classroom-1-334960 pos-abs" id="id-I1429_106">
+              <span className="classroom-1-334960-0">{"Classroom"}</span>
+            </div>
+            <div className="social-life-1-292330 pos-abs" id="id-I1429_107">
+              <span className="social-life-1-292330-0">{"Social Life"}</span>
+            </div>
+            <div className="resources-1-446019 pos-abs" id="id-I1429_108">
+              <span className="resources-1-446019-0">{"Resources"}</span>
+            </div>
+            <div className="workplace-1-173990 pos-abs" id="id-I1429_104">
+              <span className="workplace-1-173990-0">{"Workplace"}</span>
+            </div>
+            <div className="bias-awareness-1-178318 pos-abs" id="id-I1429_1010">
+              <span className="bias-awareness-1-178318-0">
+                {"Bias Awareness"}
+              </span>
+            </div>
+            <div className="header1-0-250390 pos-abs" id="id-1422"></div>
+            <div className="rectangle-1-1-202680 pos-abs" id="id-I1429_54">
+              <div
+                className="nodeBg-I1429_54 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                id="id-bg-I1429_54"
+              ></div>
+            </div>
+            <div className="rectangle-51-0-104632 pos-abs" id="id-410">
+              <div
+                className="nodeBg-410 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                id="id-bg-410"
+              >
+                {" "}
+              </div>
+            </div>
+            <div className="ellipse-51-0-747340 pos-abs" id="id-516"></div>
+            <div className="ellipse-61-0-153450 pos-abs" id="id-517"></div>
+            <div
+              className="making-educator-0-626560 pos-abs"
+              id="id-5626"
+            ></div>
+            <div className="how-does-bias-m-0-736766 pos-abs" id="id-1419">
+              <span className="how-does-bias-m-0-736766-0">
+                {"How does bias manifest in the classroom?"}
+              </span>
+            </div>
+            <div className="bias-can-manife-0-724402 pos-abs" id="id-2127">
+              <span className="bias-can-manife-0-724402-0">
+                {
+                  "Bias can manifest in the classroom through differential treatment of students by a teacher or professor. This can appear in the form of expecting students of a marginalized race, ethnicity, national origin, gender identity, sexual orientation, or socioeconomic status to not perform as well as students who are of non marginalized identities. For example, an English professor might take notice of a lot more grammar mistakes on an essay that a student whose native language isn’t English wrote and consequently end up grading harsher. This is a very common form of implicit bias in the classroom that may naturally translate to this professor subconsciously expecting less of that student (and the professor may not even be aware of this bias). Another example could be when a professor is much friendlier to her white students and ends up favoring them more, constantly showering them with encouragement and positive comments but neglecting or not taking her students of color seriously. "
+                }{" "}
+                <br /> &nbsp;{""} <br />{" "}
+                {
+                  " Bias can also appear when teachers in high school are recommending their students for advanced coursework. Statistics show that racial minority students, especially Black and Latino students, are less likely to be recommended into Advanced Placement (AP) or International Baccalaureate (IB) courses compared to their white peers. Instead, they are more likely to be recommended into lower track classes even if they have comparable grades with white students that get recommended into AP or IB courses. Additionally, high school guidance counselors may be more reluctant to encourage minority students to apply to “higher tier” schools and instead encourage them to “keep their list realistic”, implying that they should stick to applying to state schools and community colleges. When these students arrive college, they often face under preparedness in their courses along with elitism from both peers and professors. They face heightened levels of impostor syndrome, thus creating a negative feedback loop where they feel more reluctant to make effort in class because the professors have lower expectations for them. "
+                }
+              </span>
+            </div>
+            <div className="testimonials1-0-115776 pos-abs" id="id-2128">
+              <span className="testimonials1-0-115776-0">
+                {"Testimonials:"}
+              </span>
+            </div>
+            <div className="i-had-a-sociolo-0-336960 pos-abs" id="id-2129">
+              <span className="i-had-a-sociolo-0-336960-0">
+                {
+                  "“I had a sociology professor who did not take me seriously. I was one of 7 racial minority students in a class of 28 students and the only one with an accent. She had lower expectations of me and encouraged me to WX, implying that she thought I couldn’t pass the class. She was much friendlier with the white American students as well, showing signs of clear favoritism and unconscious bias.”"
+                }
+              </span>
+            </div>
+            <div className="in-one-of-my-cl-0-59314 pos-abs" id="id-2726">
+              <span className="in-one-of-my-cl-0-59314-0">
+                {
+                  "“In one of my classes sophomore year, we were learning about Muslim traditions. One of my classmates was wearing a hoodie with the hood up and she said, in front of the whole class, “you look like you could be Muslim” (insinuating that he looked like he was wearing a hijab). She then proceeded to laugh” "
+                }
+              </span>
+            </div>
+            <div className="how-can-we-comb-0-183600 pos-abs" id="id-4627">
+              <span className="how-can-we-comb-0-183600-0">
+                {"How can we combat this bias?"}
+              </span>
+            </div>
+            <div className="one-of-my-profe-0-363451 pos-abs" id="id-13627">
+              <span className="one-of-my-profe-0-363451-0">
+                {
+                  "“One of my professors kept messing up my name and the name of another student of color despite our names not even being that hard to pronounce. It took the intervention of a white student for her to finally realize she called us both by the wrong names”"
+                }
+              </span>
+            </div>
+            <div className="ellipse-41-0-298309 pos-abs" id="id-515"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("dualite-root")).render(<App />);
---
+++ resources-dualite/src/scripts.js
@@ -0,0 +1,59 @@
+function runAnimations() {
+  runAnimations_1429();
+}
+export default runAnimations;
+function runAnimations_1429() {
+  document
+    .querySelector("#id-I1429_106")
+    .addEventListener("click", func_I1429_106_36);
+
+  function func_I1429_106_36(e) {
+    e.stopPropagation();
+
+    document.querySelector("#id-1429").classList.add("animationClass-1429");
+    document
+      .querySelector("#id-I1429_106")
+      .classList.add("animationClass-I1429_106");
+    document
+      .querySelector("#id-I1429_107")
+      .classList.add("animationClass-I1429_107");
+    document
+      .querySelector("#id-I1429_108")
+      .classList.add("animationClass-I1429_108");
+    document
+      .querySelector("#id-I1429_104")
+      .classList.add("animationClass-I1429_104");
+    document
+      .querySelector("#id-I1429_1010")
+      .classList.add("animationClass-I1429_1010");
+    document.querySelector("#id-1422").classList.add("animationClass-null1422");
+    document
+      .querySelector("#id-I1429_54")
+      .classList.add("animationClass-I1429_54");
+    document.querySelector("#id-410").classList.add("animationClass-null410");
+    document
+      .querySelector("#id-410 > div:first-child")
+      .classList.add("bgClass-410");
+    document.querySelector("#id-516").classList.add("animationClass-null516");
+    document.querySelector("#id-517").classList.add("animationClass-null517");
+    document.querySelector("#id-5626").classList.add("animationClass-null5626");
+    document.querySelector("#id-1419").classList.add("animationClass-null1419");
+    document.querySelector("#id-2127").classList.add("animationClass-null2127");
+    document.querySelector("#id-2128").classList.add("animationClass-null2128");
+    document.querySelector("#id-2129").classList.add("animationClass-null2129");
+    document.querySelector("#id-2726").classList.add("animationClass-null2726");
+    document.querySelector("#id-4627").classList.add("animationClass-null4627");
+    document
+      .querySelector("#id-13627")
+      .classList.add("animationClass-null13627");
+    document.querySelector("#id-515").classList.add("animationClass-null515");
+    document
+      .querySelector("#id-I1429_106")
+      .removeEventListener("click", func_I1429_106_36);
+  }
+}
+// links object
+export const allLinks = {};
+
+// function object
+export const allFunctions = {};
