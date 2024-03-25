28ce62949d77f3cf470061ae1034c425e8aa27a3
camtaylor2 <camtaylor245@gmail.com>
2024-03-25 16:02:10 -0400

    Home Screen code

---
+++ homescreen-dualite/src/App.jsx
@@ -0,0 +1,227 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "./animation.css";
+import runAnimations, { allLinks, allFunctions } from "./scripts";
+const App = () => {
+  useEffect(() => {
+    runAnimations();
+  }, []);
+  return (
+    <div className="parent-div">
+      <div className="home-screen-1-325500 pos-abs" id="id-12">
+        <div className="header1-0-406386-container pos-abs" id="id-1422">
+          <div className="header-1-408918 pos-abs" id="id-1422">
+            <div className="classroom-1-132183 pos-abs" id="id-I1422_106">
+              <span className="classroom-1-132183-0">{"Classroom"}</span>
+            </div>
+            <div className="social-life-1-768792 pos-abs" id="id-I1422_107">
+              <span className="social-life-1-768792-0">{"Social Life"}</span>
+            </div>
+            <div className="resources-1-124800 pos-abs" id="id-I1422_108">
+              <span className="resources-1-124800-0">{"Resources"}</span>
+            </div>
+            <div className="workplace-1-255219 pos-abs" id="id-I1422_104">
+              <span className="workplace-1-255219-0">{"Workplace"}</span>
+            </div>
+            <div className="bias-awareness-1-319770 pos-abs" id="id-I1422_1010">
+              <span className="bias-awareness-1-319770-0">
+                {"Bias Awareness"}
+              </span>
+            </div>
+            <div className="header1-0-261515 pos-abs" id="id-1749"></div>
+            <div className="rectangle-1-1-548514 pos-abs" id="id-I1422_54">
+              <div
+                className="nodeBg-I1422_54 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                id="id-bg-I1422_54"
+              ></div>
+            </div>
+            <div className="rectangle-41-0-1860 pos-abs" id="id-411">
+              <div
+                className="nodeBg-411 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                id="id-bg-411"
+              >
+                {" "}
+              </div>
+            </div>
+            <div className="ellipse-61-0-42333 pos-abs" id="id-519"></div>
+            <div className="ellipse-72-0-166085 pos-abs" id="id-520"></div>
+            <div className="how-does-bias-m-0-86976 pos-abs" id="id-9826">
+              <span className="how-does-bias-m-0-86976-0">
+                {"How does bias manifest in our social lives?"}
+              </span>
+            </div>
+            <div className="how-can-we-comb-0-356088 pos-abs" id="id-9827">
+              <span className="how-can-we-comb-0-356088-0">
+                {"How can we combat this bias?"}
+              </span>
+            </div>
+            <div className="biases-in-socia-0-487025 pos-abs" id="id-13126">
+              <span className="biases-in-socia-0-487025-0">
+                {
+                  "Biases in social settings often ostracize minorities due to homophily and in-group preference. If an individual views another individual as a member of their group, he/she is more likely to be positively biased and attracted to that person. There will also be a stronger feeling of loyalty to that person. On the contrary, an individual is indifferent to or even negatively disposed to those that are perceived as not a member to their group, or a member of an out-group. For example, when it comes to race, people unconsciously tend to hangout with those that look like them. This is because they usually have cultural similarities. Therefore, when in a setting that is predominantly occupied by a certain race, those that are not a part of that same race may struggle to build connections and are left outside of the social circle. Studies show that there also becomes an increased fear and dislike of those in the out-group due to those in the in-group never gaining comfortability with out-group members.  "
+                }{" "}
+                <br />{" "}
+                {
+                  " Individuals that identify as LGBTQ+ may also face exclusion from certain social gatherings. It is important for these individuals to have safe spaces made for them due to the alarming risk of harassment and violence towards them. However, it is also important for LGBTQ+ individuals to feel accepted and respected in all spaces in order to not feel alienated. Stereotypes and misconceptions about LGBTQ+ individuals leads to even further isolation.  "
+                }{" "}
+                <br />{" "}
+              </span>
+            </div>
+            <div
+              className="cultivate-empat-0-922450 pos-abs"
+              id="id-13530"
+            ></div>
+            <div className="on-my-sports-te-0-76670 pos-abs" id="id-13531">
+              <span className="on-my-sports-te-0-76670-0">
+                {
+                  "“On my sports team, I am one of two of the only minority individuals. Whenever the team would plan to attend a frat party together, her and I were always the last to be told about it. It made us feel like we weren’t really apart of the group socially.”"
+                }
+              </span>
+            </div>
+            <div className="as-a-black-woma-0-564446 pos-abs" id="id-13532">
+              <span className="as-a-black-woma-0-564446-0">
+                {
+                  "“As a black woman, when I am the only black person in a social setting, I feel invisible. There have been countless times where even if I try to join a conversation in a regular space outside of the classroom, my presence is not given the same energy my white female friends are given” "
+                }
+              </span>
+            </div>
+            <div className="the-older-i-got-0-234612 pos-abs" id="id-13533">
+              <span className="the-older-i-got-0-234612-0">
+                {
+                  "“The older I got the more I realized that I only had friends that were similar to me in race, gender, and sexual identity. After coming to college and opening up my perspective, I have tried to acknowledge my biases and build connections with others that aren’t apart of my same bubble”"
+                }
+              </span>
+            </div>
+            <div className="testimonials1-0-41666 pos-abs" id="id-13537">
+              <span className="testimonials1-0-41666-0">{"Testimonials:"}</span>
+            </div>
+            <div className="credit istock-b-0-358708 pos-abs" id="id-15427">
+              <span className="credit istock-b-0-358708-0">
+                {"Credit: iStock by Getty Images"}
+              </span>
+            </div>
+            <div className="ellipse-71-0-573402 pos-abs" id="id-518"></div>
+          </div>
+        </div>
+        <div className="image-1-1-646550 pos-abs" id="id-10828">
+          <div
+            className="nodeBg-10828 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+            id="id-bg-10828"
+          >
+            {" "}
+          </div>
+        </div>
+        <div className="rectangle-51-0-595884 pos-abs" id="id-410">
+          <div
+            className="nodeBg-410 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+            id="id-bg-410"
+          >
+            {" "}
+          </div>
+        </div>
+        <div className="are-you-conscio-1-99470 pos-abs" id="id-10527">
+          <span className="are-you-conscio-1-99470-0">
+            {" "}
+            <br /> {" Are you conscious of your unconscious bias?"}
+          </span>
+        </div>
+        <div className="learn-about-how-1-172494 pos-abs" id="id-13536">
+          <span className="learn-about-how-1-172494-0">
+            {
+              "Learn about how these biases affect the workplace, classroom, and social life- "
+            }
+            &nbsp;{""} <br /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            {" and how we can combat them."}
+          </span>
+        </div>
+        <div className="unconscious-bia-1-169068 pos-abs" id="id-14026">
+          <span className="unconscious-bia-1-169068-0">
+            {
+              "Unconscious biases are social stereotypes about certain groups of people that individuals form outside their own conscious awareness. Everyone holds unconscious beliefs about various social and identity groups. "
+            }
+            &nbsp;{""} <br />{" "}
+          </span>
+        </div>
+        <div className="credit lyubov-i-1-170286 pos-abs" id="id-15426">
+          <span className="credit lyubov-i-1-170286-0">
+            {"Credit: Lyubov Ivanova Getty Images"}
+          </span>
+        </div>
+        <div className="header-1-50388 pos-abs" id="id-1421">
+          <div className="classroom-1-406995 pos-abs" id="id-106">
+            <span className="classroom-1-406995-0">{"Classroom"}</span>
+          </div>
+          <div className="social-life-1-6930 pos-abs" id="id-107">
+            <span className="social-life-1-6930-0">{"Social Life"}</span>
+          </div>
+          <div className="resources-1-725708 pos-abs" id="id-108">
+            <span className="resources-1-725708-0">{"Resources"}</span>
+          </div>
+          <div className="workplace-1-156480 pos-abs" id="id-104">
+            <span className="workplace-1-156480-0">{"Workplace"}</span>
+          </div>
+          <div className="bias-awareness-1-56376 pos-abs" id="id-1010">
+            <span className="bias-awareness-1-56376-0">{"Bias Awareness"}</span>
+          </div>
+          <div className="rectangle-1-1-21899 pos-abs" id="id-54">
+            <div
+              className="nodeBg-54 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+              id="id-bg-54"
+            ></div>
+          </div>
+        </div>
+        <div className="ellipse-51-0-373632 pos-abs" id="id-516"></div>
+        <div className="ellipse-61-0-163875 pos-abs" id="id-517"></div>
+        <div className="making-educator-0-66330 pos-abs" id="id-5626"></div>
+        <div className="how-does-bias-m-0-641456 pos-abs" id="id-1419">
+          <span className="how-does-bias-m-0-641456-0">
+            {"How does bias manifest in the classroom?"}
+          </span>
+        </div>
+        <div className="bias-can-manife-0-269777 pos-abs" id="id-2127">
+          <span className="bias-can-manife-0-269777-0">
+            {
+              "Bias can manifest in the classroom through differential treatment of students by a teacher or professor. This can appear in the form of expecting students of a marginalized race, ethnicity, national origin, gender identity, sexual orientation, or socioeconomic status to not perform as well as students who are of non marginalized identities. For example, an English professor might take notice of a lot more grammar mistakes on an essay that a student whose native language isn’t English wrote and consequently end up grading harsher. This is a very common form of implicit bias in the classroom that may naturally translate to this professor subconsciously expecting less of that student (and the professor may not even be aware of this bias). Another example could be when a professor is much friendlier to her white students and ends up favoring them more, constantly showering them with encouragement and positive comments but neglecting or not taking her students of color seriously. "
+            }{" "}
+            <br /> &nbsp;{""} <br />{" "}
+            {
+              " Bias can also appear when teachers in high school are recommending their students for advanced coursework. Statistics show that racial minority students, especially Black and Latino students, are less likely to be recommended into Advanced Placement (AP) or International Baccalaureate (IB) courses compared to their white peers. Instead, they are more likely to be recommended into lower track classes even if they have comparable grades with white students that get recommended into AP or IB courses. Additionally, high school guidance counselors may be more reluctant to encourage minority students to apply to “higher tier” schools and instead encourage them to “keep their list realistic”, implying that they should stick to applying to state schools and community colleges. When these students arrive college, they often face under preparedness in their courses along with elitism from both peers and professors. They face heightened levels of impostor syndrome, thus creating a negative feedback loop where they feel more reluctant to make effort in class because the professors have lower expectations for them. "
+            }
+          </span>
+        </div>
+        <div className="testimonials1-0-136425 pos-abs" id="id-2128">
+          <span className="testimonials1-0-136425-0">{"Testimonials:"}</span>
+        </div>
+        <div className="i-had-a-sociolo-0-413913 pos-abs" id="id-2129">
+          <span className="i-had-a-sociolo-0-413913-0">
+            {
+              "“I had a sociology professor who did not take me seriously. I was one of 7 racial minority students in a class of 28 students and the only one with an accent. She had lower expectations of me and encouraged me to WX, implying that she thought I couldn’t pass the class. She was much friendlier with the white American students as well, showing signs of clear favoritism and unconscious bias.”"
+            }
+          </span>
+        </div>
+        <div className="in-one-of-my-cl-0-215605 pos-abs" id="id-2726">
+          <span className="in-one-of-my-cl-0-215605-0">
+            {
+              "“In one of my classes sophomore year, we were learning about Muslim traditions. One of my classmates was wearing a hoodie with the hood up and she said, in front of the whole class, “you look like you could be Muslim” (insinuating that he looked like he was wearing a hijab). She then proceeded to laugh” "
+            }
+          </span>
+        </div>
+        <div className="how-can-we-comb-0-71690 pos-abs" id="id-4627">
+          <span className="how-can-we-comb-0-71690-0">
+            {"How can we combat this bias?"}
+          </span>
+        </div>
+        <div className="one-of-my-profe-0-36045 pos-abs" id="id-13627">
+          <span className="one-of-my-profe-0-36045-0">
+            {
+              "“One of my professors kept messing up my name and the name of another student of color despite our names not even being that hard to pronounce. It took the intervention of a white student for her to finally realize she called us both by the wrong names”"
+            }
+          </span>
+        </div>
+        <div className="ellipse-41-0-559968 pos-abs" id="id-515"></div>
+      </div>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("dualite-root")).render(<App />);
---
+++ homescreen-dualite/src/scripts.js
@@ -0,0 +1,105 @@
+function runAnimations() {
+  runAnimations_1422();
+
+  document.querySelector("#id-106").addEventListener("click", func_106_36);
+
+  function func_106_36(e) {
+    e.stopPropagation();
+
+    document.querySelector("#id-12").classList.add("animationClass-12");
+    document.querySelector("#id-1422").classList.add("animationClass-null1422");
+    document.querySelector("#id-10828").classList.add("animationClass-10828");
+    document.querySelector("#id-410").classList.add("animationClass-null410");
+    document
+      .querySelector("#id-410 > div:first-child")
+      .classList.add("bgClass-410");
+    document.querySelector("#id-10527").classList.add("animationClass-10527");
+    document.querySelector("#id-13536").classList.add("animationClass-13536");
+    document.querySelector("#id-14026").classList.add("animationClass-14026");
+    document.querySelector("#id-15426").classList.add("animationClass-15426");
+    document.querySelector("#id-1421").classList.add("animationClass-1421");
+    document.querySelector("#id-516").classList.add("animationClass-null516");
+    document.querySelector("#id-517").classList.add("animationClass-null517");
+    document.querySelector("#id-5626").classList.add("animationClass-null5626");
+    document.querySelector("#id-1419").classList.add("animationClass-null1419");
+    document.querySelector("#id-2127").classList.add("animationClass-null2127");
+    document.querySelector("#id-2128").classList.add("animationClass-null2128");
+    document.querySelector("#id-2129").classList.add("animationClass-null2129");
+    document.querySelector("#id-2726").classList.add("animationClass-null2726");
+    document.querySelector("#id-4627").classList.add("animationClass-null4627");
+    document
+      .querySelector("#id-13627")
+      .classList.add("animationClass-null13627");
+    document.querySelector("#id-515").classList.add("animationClass-null515");
+    document.querySelector("#id-106").removeEventListener("click", func_106_36);
+  }
+}
+export default runAnimations;
+function runAnimations_1422() {
+  document
+    .querySelector("#id-I1422_107")
+    .addEventListener("click", func_I1422_107_37);
+
+  function func_I1422_107_37(e) {
+    e.stopPropagation();
+
+    document.querySelector("#id-1422").classList.add("animationClass-1422");
+    document
+      .querySelector("#id-I1422_106")
+      .classList.add("animationClass-I1422_106");
+    document
+      .querySelector("#id-I1422_107")
+      .classList.add("animationClass-I1422_107");
+    document
+      .querySelector("#id-I1422_108")
+      .classList.add("animationClass-I1422_108");
+    document
+      .querySelector("#id-I1422_104")
+      .classList.add("animationClass-I1422_104");
+    document
+      .querySelector("#id-I1422_1010")
+      .classList.add("animationClass-I1422_1010");
+    document.querySelector("#id-1749").classList.add("animationClass-null1749");
+    document
+      .querySelector("#id-I1422_54")
+      .classList.add("animationClass-I1422_54");
+    document.querySelector("#id-411").classList.add("animationClass-null411");
+    document
+      .querySelector("#id-411 > div:first-child")
+      .classList.add("bgClass-411");
+    document.querySelector("#id-519").classList.add("animationClass-null519");
+    document.querySelector("#id-520").classList.add("animationClass-null520");
+    document.querySelector("#id-9826").classList.add("animationClass-null9826");
+    document.querySelector("#id-9827").classList.add("animationClass-null9827");
+    document
+      .querySelector("#id-13126")
+      .classList.add("animationClass-null13126");
+    document
+      .querySelector("#id-13530")
+      .classList.add("animationClass-null13530");
+    document
+      .querySelector("#id-13531")
+      .classList.add("animationClass-null13531");
+    document
+      .querySelector("#id-13532")
+      .classList.add("animationClass-null13532");
+    document
+      .querySelector("#id-13533")
+      .classList.add("animationClass-null13533");
+    document
+      .querySelector("#id-13537")
+      .classList.add("animationClass-null13537");
+    document
+      .querySelector("#id-15427")
+      .classList.add("animationClass-null15427");
+    document.querySelector("#id-518").classList.add("animationClass-null518");
+    document
+      .querySelector("#id-I1422_107")
+      .removeEventListener("click", func_I1422_107_37);
+  }
+}
+// links object
+export const allLinks = {};
+
+// function object
+export const allFunctions = {};
