187ccbcea98c2a8dfb3683c7f583e8c45802588b
qinghu1 <qingqinghu279@gmail.com>
2024-03-25 16:07:47 -0400

    Put the code for workplace page

---
+++ workplace-dualite/src/scripts.js
@@ -0,0 +1,63 @@
+function runAnimations() {
+  runAnimations_1738();
+}
+export default runAnimations;
+function runAnimations_1738() {
+  document.querySelector("#id-1738").addEventListener("click", func_1738_35);
+
+  function func_1738_35(e) {
+    e.stopPropagation();
+
+    document.querySelector("#id-1738").classList.add("animationClass-1738");
+    document
+      .querySelector("#id-I1738_106")
+      .classList.add("animationClass-I1738_106");
+    document
+      .querySelector("#id-I1738_107")
+      .classList.add("animationClass-I1738_107");
+    document
+      .querySelector("#id-I1738_108")
+      .classList.add("animationClass-I1738_108");
+    document
+      .querySelector("#id-I1738_104")
+      .classList.add("animationClass-I1738_104");
+    document
+      .querySelector("#id-I1738_1010")
+      .classList.add("animationClass-I1738_1010");
+    document.querySelector("#id-1738").classList.add("animationClass-null1738");
+    document
+      .querySelector("#id-I1738_54")
+      .classList.add("animationClass-I1738_54");
+    document.querySelector("#id-511").classList.add("animationClass-null511");
+    document
+      .querySelector("#id-511 > div:first-child")
+      .classList.add("bgClass-511");
+    document.querySelector("#id-513").classList.add("animationClass-null513");
+    document.querySelector("#id-514").classList.add("animationClass-null514");
+    document.querySelector("#id-9629").classList.add("animationClass-null9629");
+    document.querySelector("#id-9628").classList.add("animationClass-null9628");
+    document.querySelector("#id-1417").classList.add("animationClass-null1417");
+    document.querySelector("#id-9527").classList.add("animationClass-null9527");
+    document
+      .querySelector("#id-10236")
+      .classList.add("animationClass-null10236");
+    document
+      .querySelector("#id-13828")
+      .classList.add("animationClass-null13828");
+    document
+      .querySelector("#id-13831")
+      .classList.add("animationClass-null13831");
+    document
+      .querySelector("#id-13832")
+      .classList.add("animationClass-null13832");
+    document.querySelector("#id-512").classList.add("animationClass-null512");
+    document
+      .querySelector("#id-1738")
+      .removeEventListener("click", func_1738_35);
+  }
+}
+// links object
+export const allLinks = {};
+
+// function object
+export const allFunctions = {};
